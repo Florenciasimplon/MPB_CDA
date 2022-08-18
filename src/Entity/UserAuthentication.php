e1eca09fb4c1fae499b344cf33b64acbd21623ec
Florencia <florenciasimplon@gmail.com>
2022-08-18 12:02:42 +0200

    nouveau projet + JWT

---
+++ src/Entity/UserAuthentication.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserAuthenticationRepository;
+use ApiPlatform\Core\Action\NotFoundAction;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Controller\MeController;
+use Doctrine\ORM\Mapping as ORM;
+use Lcobucci\JWT\Signer\None;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Annotation\SerializedName;
+use Symfony\Component\Validator\Constraints\IsNull;
+
+#[ORM\Entity(repositoryClass: UserAuthenticationRepository::class)]
+#[ApiResource(
+    collectionOperations: [
+        'get' => ['method' => 'get'],
+        'post' => ['method' => 'post'],
+    ],
+    itemOperations: [
+        'get' => ['method' => 'get', 'requirements' => ['id' => '\d+'],],
+        'put' => ['method' => 'put'],
+        'delete' => ['method' => 'delete']
+    ],
+)]
+class UserAuthentication implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    private $email;
+
+    #[ORM\Column(type: 'json')]
+    private $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    private $password;
+
+    #[ApiProperty(
+        attributes: [
+            "openapi_context" => [
+                "type" => "string",
+                "example" => "password",
+            ],
+        ],
+    )]
+    private $plainPassword;
+    
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $Picture;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $UserName;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getPlainPassword(): string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+    public function getPicture(): ?string
+    {
+        return $this->Picture;
+    }
+
+    public function setPicture(?string $Picture): self
+    {
+        $this->Picture = $Picture;
+
+        return $this;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->UserName;
+    }
+
+    public function setUserName(string $UserName): self
+    {
+        $this->UserName = $UserName;
+
+        return $this;
+    }
+}
